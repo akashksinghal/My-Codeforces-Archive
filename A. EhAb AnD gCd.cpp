b15e859e3c64d4fd4af76e31a53e8bf2df58dd78
Akash Kumar Singhal <akashksinghalnewdelhi@gmail.com>
2020-03-30 02:18:56 +0530

    Commit

---
+++ A. EhAb AnD gCd.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ascend(A) sort(A.begin(),A.end())
+#define input(A,N) for(ll i=0;i<N;i++) scanf("%lld",&A[i]);
+void solve()
+{
+    ll N;
+    cin>>N;
+    cout<<1<<" "<<N-1<<endl;
+}
+int main()
+{
+    ll testcase;
+    scanf("%lld",&testcase);
+    while(testcase--)
+    {
+        solve();
+    }
+    return 0;
+}
