33874d934234eda851b95ee3b0535369651ae838
Akash Kumar Singhal <akashksinghalnewdelhi@gmail.com>
2020-04-13 19:49:39 +0530

    1334

---
+++ f1.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fasino ios_base::sync_with_stdio(false); cin.tie(0);
+#define asc(A) sort(A.begin(),A.end())
+#define dsc(A) sort(A.begin(),A.end(),greater<ll>())
+#define input(A,N) for(ll i=0;i<N;i++) cin>>A[i];
+void solve()
+{
+    
+}
+int main()
+{
+    fasino
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); 
+    #endif
+    int testcase;
+    cin>>testcase;
+    while(testcase--)
+    {
+        solve();
+    }
+    return 0;
+}
